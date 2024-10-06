a9317803751cb38d2ba26559fe5188108437102b
john beltrez <beltrezjohn1@gmail.com>
2024-10-06 18:36:29 -0500

    completed second function

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function sumArr(arr) {
     return arr.reduce((accumulator, element) => accumulator + element, 0)
 }
+// take an array of numbers and return the average
+function avgArr(arr) {
+    return arr.reduce((accumulator, element) => accumulator + element, 0) / arr.length;
+}
+
+// const avg = avgArr([1, 2, 3, 4, 5]);
+
+// console.log(avg);
+
+// 
