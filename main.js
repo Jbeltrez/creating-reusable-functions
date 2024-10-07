abf497d46ad3983b4dd51e47323603f05924290c
john beltrez <beltrezjohn1@gmail.com>
2024-10-06 22:05:22 -0500

    function 3 completed

--- main.js
+++ main.js
@@ -9,5 +9,22 @@ function avgArr(arr) {
 // const avg = avgArr([1, 2, 3, 4, 5]);
 
 // console.log(avg);
+// take an array of strings and return the longest string
+function findLongest(arr) {
+    return arr.reduce((accumulator, element) => {
+        if (element.length > accumulator.length) {
+            // console.log(element)
+            return element
+            
+        } else {
+            // console.log(accumulator)
+             return accumulator
+        }
+    }, "");
+}
 
-// 
+
+const strs = ['one', 'two', 'three', 'four'];
+console.log(findLongest(strs)) 
+
+// console.log(str)
